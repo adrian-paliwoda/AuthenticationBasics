fb14e951380eb53830b15ab1f7c6e7c6e44382a3
Adrian Paliwoda <adrian94@tutanota.com>
2026-06-01 21:05:50 +0200

    Wrong cookie auth

--- AuthenticationBasicsApp/Controllers/AuthenticationController.cs
+++ AuthenticationBasicsApp/Controllers/AuthenticationController.cs
@@ -8,10 +8,21 @@ namespace AuthenticationBasicsApp.Controllers;
 [Route("api/auth")]
 public class AuthenticationController : ControllerBase
 {
+    [HttpGet("username")]
+    public ActionResult UserName()
+    {
+        if (HttpContext.Request.Cookies.TryGetValue("username", out var username))
+        {
+            return Ok(username);
+        }
+        
+        return NotFound("There is no such username");
+    }
+    
     [HttpGet("login")]
     public IActionResult Login()
     {
-        HttpContext.Response.Headers.Append("set-cookie", "auth=user:adrian");
+        HttpContext.Response.Cookies.Append("auth", "user:adrian");
         return Ok();
     }
 
